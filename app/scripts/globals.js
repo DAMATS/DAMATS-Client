c76a594a62ae830e65208673fa27703318bd663a
Martin Paces <martin.paces@eox.at>
2016-11-15 17:49:44 +0100

    Version update.

--- app/scripts/globals.js
+++ app/scripts/globals.js
@@ -35,7 +35,7 @@ define(
     ],
     function (Backbone, ObjectStore, DataModels) {
         return {
-            version: "0.3.1dev",
+            version: "0.3.2dev",
             objects: new ObjectStore(),
             selections: new ObjectStore(),
             baseLayers: new Backbone.Collection(),
